3df78cb3f009743cd986b92001893c295458bb66
Robi <tttrobert13@gmail.com>
2024-12-07 16:48:58 +0200

    Delete PoliHack directory

--- PoliHack/src/main/java/com/example/PoliHack/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.PoliHack.repository;
-
-import com.example.PoliHack.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-}
