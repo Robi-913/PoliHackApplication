67239eb6cc4bcd0efea022b89da3438f6b0cacdf
Robi <tttrobert13@gmail.com>
2024-12-07 15:25:41 +0200

    user login/ register

--- PoliHack/src/main/java/com/example/PoliHack/repository/UserRepository.java
+++ PoliHack/src/main/java/com/example/PoliHack/repository/UserRepository.java
@@ -3,5 +3,6 @@ package com.example.PoliHack.repository;
 import com.example.PoliHack.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, Integer> {
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByNickname(String nickname);
 }
--- PoliHack/src/main/java/com/example/PoliHack/service/UserService.java
+++ PoliHack/src/main/java/com/example/PoliHack/service/UserService.java
@@ -2,12 +2,13 @@ package com.example.PoliHack.service;
 
 import com.example.PoliHack.model.User;
 import com.example.PoliHack.repository.UserRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private final UserRepository userRepository;
 
@@ -15,7 +16,27 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void saveUser(User user) {
-        userRepository.save(user);
+    public boolean authenticateUser(String nickname, String password) {
+        User user = userRepository.findByNickname(nickname);
+        return user != null && user.getPassword().equals(password);
     }
+
+    public boolean registerUser(User user) {
+        if (user.getId() == null || user.getId().isEmpty()) {
+            user.setId(new ObjectId().toString());
+        }
+
+        if (userRepository.findByNickname(user.getNickname()) != null) {
+            return false;
+        }
+
+        if (user.getPassword() == null || user.getPassword().isEmpty()) {
+            return false;
+        }
+
+        userRepository.save(user);
+        return true;
+    }
+
 }
+
