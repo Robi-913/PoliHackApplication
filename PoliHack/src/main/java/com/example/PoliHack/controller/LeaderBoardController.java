508a626a10184385bfdf11594aa5f0c5bf2ffc3a
Robi <tttrobert13@gmail.com>
2024-12-07 21:11:43 +0200

    user

--- PoliHack/src/main/java/com/example/PoliHack/controller/LeaderBoardController.java
+++ PoliHack/src/main/java/com/example/PoliHack/controller/LeaderBoardController.java
@@ -1,7 +1,6 @@
 package com.example.PoliHack.controller;
 
 import com.example.PoliHack.service.LeaderBoardService;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
