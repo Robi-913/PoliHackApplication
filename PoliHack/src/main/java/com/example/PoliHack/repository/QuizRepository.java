dd1afc55fea8a86034c72d2516d151ac11d0bac6
Robi <tttrobert13@gmail.com>
2025-01-03 01:30:54 +0200

    Delete PoliHack directory

--- PoliHack/src/main/java/com/example/PoliHack/repository/QuizRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.PoliHack.repository;
-
-import com.example.PoliHack.model.Quiz;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface QuizRepository extends MongoRepository<Quiz, ObjectId> {
-}
--- PoliHack/src/main/java/com/example/PoliHack/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.PoliHack.repository;
-
-import com.example.PoliHack.model.user.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByNickname(String nickname);
-}
